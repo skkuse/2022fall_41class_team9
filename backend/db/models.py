a022b86a16dc4bffd14ac9a33c363e7014210f68
metishonora <metishonora@icloud.com>
2022-11-12 21:39:32 +0900

    Modify models
    Course class has been added.
    Testcase class had been merged into class Problem.
    Submission counters have been added.
    All fields in class Analysis have been changed to TextFields.

--- backend/db/models.py
+++ backend/db/models.py
@@ -3,66 +3,62 @@ from django.contrib.postgres.fields import ArrayField
 from django.db.models.deletion import CASCADE
 from django.db.models.enums import Choices
 
-"""Todo
-1. Pair programming for verification.
-"""
+
+class Course(models.Model):
+    course_id = models.IntegerField(primary_key=True)
+    course_name = models.CharField(max_length=50)
+
 
 class User(models.Model):
     THEME = [
-        ('Dark', {'background': 'black', 'color': 'white'}),
-        ('Light', {'background': 'white', 'color': 'black'}),
+        ("Dark", {"background": "black", "color": "white"}),
+        ("Light", {"background": "white", "color": "black"}),
     ]
     FONT = [
-        ('A', 'Arial'),
-        ('C', 'Consolas'),
+        ("A", "Arial"),
+        ("C", "Consolas"),
     ]
 
     user_id = models.IntegerField(primary_key=True)
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
-    course = ArrayField(ArrayField(models.CharField(max_length=50)))
-    setting_theme = models.CharField(choices=THEME, default='Light')
-    setting_font = models.CharField(choices=FONT, default='C')
+    course_id = models.ForeignKey(
+        Course, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    setting_theme = models.CharField(choices=THEME, default="Light")
+    setting_font = models.CharField(choices=FONT, default="C")
+
 
 class Problem(models.Model):
     prob_id = models.IntegerField(primary_key=True)
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
     writer = models.CharField(max_length=50)
     title = models.CharField(max_length=50)
     tag = ArrayField(ArrayField(models.CharField(max_length=20)))
     description = models.TextField()
     constraint = models.TextField(blank=True)
+    prob_code = models.TextField()
+    answer_code = models.TextField()
     skeleton = models.TextField(blank=True)
     max_submission = models.IntegerField()
     deadline = models.DateField()
+    tc_open = models.TextField()
+    tc_close = models.TextField()
 
-"""Todo
-1. Result of checking (among Execute / Check / Submit)?
-"""
+
 class Submission(models.Model):
     submit_id = models.IntegerField(primary_key=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    prob_id = models.ForeignKey(Problem, on_delete=models.SET_NULL, null=True, blank=True)
-    #answer?
-    code = models.TextField(blank=True)
-    output = models.TextField(blank=True)
+    prob_id = models.ForeignKey(Problem, on_delete=models.CASCADE)
+    user_code = models.TextField(blank=True)
+    user_output = models.TextField(blank=True)
+    counter = models.IntegerField()
 
-#class sub-count is not implemented -- can be replaced by a query.
 
-"""Todo
-1. Subfields in efficiency field.
-2. Subfileds in readability field.
-3. The way to detect plagiarism.
-"""
 class Analysis(models.Model):
     submit_id = models.ForeignKey(Submission, on_delete=models.CASCADE)
-    efficiency = models.IntegerField()
-    readability = models.IntegerField()
-    plagiarism = models.IntegerField()
+    efficiency = models.TextField(blank=True)
+    readability = models.TextField(blank=True)
+    plagiarism = models.TextField(blank=True)
     explanation = models.TextField(blank=True)
-
-class Testcase(models.Model):
-    tc_id = models.IntegerField(primary_key=True)
-    prob_id = models.ForeignKey(Problem, on_delete=models.CASCADE)
-    opened = models.BooleanField()
-    stdin = models.TextField()
-    expected_out = models.TextField()
+    funtionability = models.TextField(blank=True)
