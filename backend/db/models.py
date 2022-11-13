aac3cdcbee9af65b35d044bef01781aefa2e50e9
metishonora <metishonora@icloud.com>
2022-11-13 19:17:10 +0900

    Solve migration error
    See #2.

--- backend/db/models.py
+++ backend/db/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.postgres.fields import ArrayField
 from django.db.models.deletion import CASCADE
 from django.db.models.enums import Choices
 
@@ -10,35 +9,39 @@ class Course(models.Model):
 
 
 class User(models.Model):
+    DARK = "Dark"
+    LIGHT = "Light"
+    A = "A"
+    C = "C"
     THEME = [
-        ("Dark", {"background": "black", "color": "white"}),
-        ("Light", {"background": "white", "color": "black"}),
+        (DARK, "{'background': 'black', 'color': 'white'}"),
+        (LIGHT, "{'background': 'white', 'color': 'black'}"),
     ]
     FONT = [
-        ("A", "Arial"),
-        ("C", "Consolas"),
+        (A, "Arial"),
+        (C, "Consolas"),
     ]
 
     user_id = models.IntegerField(primary_key=True)
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
     course_id = models.ForeignKey(
-        Course, on_delete=models.SET_NULL, null=True, blank=True
+        'Course', on_delete=models.SET_NULL, null=True, blank=True
     )
-    setting_theme = models.CharField(choices=THEME, default="Light", max_length=50)
-    setting_font = models.CharField(choices=FONT, default="C", max_length=50)
+    setting_theme = models.CharField(choices=THEME, default=LIGHT, max_length=50)
+    setting_font = models.CharField(choices=FONT, default=C, max_length=50)
 
 
 class Problem(models.Model):
     prob_id = models.IntegerField(primary_key=True)
-    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+    course_id = models.ForeignKey('Course', on_delete=models.CASCADE)
     writer = models.CharField(max_length=50)
     title = models.CharField(max_length=50)
-    tag = ArrayField(ArrayField(models.CharField(max_length=20)))
+    tag = models.TextField()
     description = models.TextField()
-    constraint = models.TextField(blank=True)
+    constraint = models.TextField()
     answer_code = models.TextField()
-    skeleton = models.TextField(blank=True)
+    skeleton = models.TextField()
     max_submission = models.IntegerField()
     deadline = models.DateField()
     tc_open = models.TextField()
@@ -47,17 +50,18 @@ class Problem(models.Model):
 
 class Submission(models.Model):
     submit_id = models.IntegerField(primary_key=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    prob_id = models.ForeignKey(Problem, on_delete=models.CASCADE)
-    user_code = models.TextField(blank=True)
-    user_output = models.TextField(blank=True)
+    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
+    prob_id = models.ForeignKey('Problem', on_delete=models.CASCADE)
+    user_code = models.TextField()
+    user_output = models.TextField()
     counter = models.IntegerField()
 
 
 class Analysis(models.Model):
-    submit_id = models.ForeignKey(Submission, on_delete=models.CASCADE)
+    submit_id = models.ForeignKey('Submission', on_delete=models.CASCADE)
     efficiency = models.TextField(blank=True)
     readability = models.TextField(blank=True)
     plagiarism = models.FloatField()
     explanation = models.TextField(blank=True)
     functionability = models.TextField(blank=True)
+
