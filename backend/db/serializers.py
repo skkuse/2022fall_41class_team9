ffd14c9d65e7b751e60ea98a2b03372eba2493e3
metishonora <metishonora@icloud.com>
2022-11-15 00:03:14 +0900

    Add unpushed changes before merge

--- backend/db/serializers.py
+++ backend/db/serializers.py
@@ -1,29 +1,37 @@
 from rest_framework import serializers
 
-from db.models import Analysis, Course, Problem, Submission, User
+from db.models import Course
+from db.models import User
+from db.models import Problem
+from db.models import Submission
+from db.models import Analysis
+
 
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = "__all__"
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
+
 
 class ProblemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Problem
         fields = "__all__"
 
+
 class SubmissionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Submission
         fields = "__all__"
 
+
 class AnalysisSerializer(serializers.ModelSerializer):
     class Meta:
         model = Analysis
         fields = "__all__"
-
