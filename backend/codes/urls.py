c7fc27600c18fa3a96fad6e54afe649ae3ed98c6
Nickel <stbaker517@naver.com>
2022-11-14 17:31:15 +0000

    codes view test

--- backend/codes/urls.py
+++ backend/codes/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path,include
+from .views import *
 
 urlpatterns =[
     #path('todo/',TodosAPIView.as_view()),
-
+    path('user/',UserAPIView.as_view()),
+    path('course/',CourseAPIView.as_view()),
 ]
--- backend/codes/views.py
+++ backend/codes/views.py
@@ -1,3 +1,57 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import viewsets
+from rest_framework.generics import get_object_or_404
 
+import sys
+sys.path.append("..")
+from db.models import Submission
+from db.serializers import *
 # Create your views here.
+# class Submission(models.Model):
+#     submit_id = models.IntegerField(primary_key=True)
+#     user_id = models.ForeignKey('User', on_delete=models.CASCADE)
+#     prob_id = models.ForeignKey('Problem', on_delete=models.CASCADE)
+#     user_code = models.TextField()
+#     user_output = models.TextField()
+#     counter = models.IntegerField()
+
+class SubmissionsAPIView(APIView):
+
+
+    def post(self, request, submit_id,user_id,prob_id,user_code,user_output,counter):
+        serializer = SubmissionSerializer(data=request.data)
+
+
+
+class SubmissionsByUserIdAPIView(APIView):
+    def get(self,request,user_id):
+        submissions = Submission.objects.filter(user_id=user_id)
+        
+
+class UserAPIView(APIView):
+    def get(self,request):
+        users=User.objects.all()
+        serializer = UserSerializer(users,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    def post(self,request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.data,status=status.HTTP_400_BAD_REQUEST)
+
+class CourseAPIView(APIView):
+    def get(self,request):
+        courses=Course.objects.all()
+        serializer = CourseSerializer(courses,many=True)
+        return Response(serializer.error,status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = CourseSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.error,status=status.HTTP_400_BAD_REQUEST)
