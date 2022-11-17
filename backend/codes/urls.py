eb85a49f0c3324bc03db00ecebad2be3c1895577
Nickel <stbaker517@naver.com>
2022-11-17 21:40:35 +0000

    basic api done

--- backend/codes/urls.py
+++ backend/codes/urls.py
@@ -3,6 +3,13 @@ from .views import *
 
 urlpatterns =[
     #path('todo/',TodosAPIView.as_view()),
-    path('user/',UserAPIView.as_view()),
-    path('course/',CourseAPIView.as_view()),
+    path('users/',UsersAPIView.as_view()),
+    path('user/<int:pk>',UserAPIView.as_view()),
+    path('courses/',CoursesAPIView.as_view()),
+    path('course/<int:pk>',CourseAPIView.as_view()),
+    path('problems/',ProblemsAPIView.as_view()),
+    path('problem/<int:pk>',ProblemAPIView.as_view()),
+    path('submission/',SubmissionAPIView.as_view()),
+    path('submissions/',SubmissionsAPIView.as_view()),
+    #path()
 ]
--- backend/codes/views.py
+++ backend/codes/views.py
@@ -31,27 +31,93 @@ class SubmissionsByUserIdAPIView(APIView):
         submissions = Submission.objects.filter(user_id=user_id)
         
 
-class UserAPIView(APIView):
+class UsersAPIView(APIView):
     def get(self,request):
         users=User.objects.all()
         serializer = UserSerializer(users,many=True)
         return Response(serializer.data,status=status.HTTP_200_OK)
-    def post(self,request):
-        serializer = UserSerializer(data=request.data)
+    def post (self,request):
+        serializer=ProblemSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return Response(serializer.data,status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.error,status=status.HTTP_400_BAD_REQUEST)
 
-class CourseAPIView(APIView):
+class UserAPIView(APIView):
+    def get(self,request,pk):
+        user=get_object_or_404(User,user_id=pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+
+class CoursesAPIView(APIView):
     def get(self,request):
         courses=Course.objects.all()
         serializer = CourseSerializer(courses,many=True)
-        return Response(serializer.error,status=status.HTTP_200_OK)
-
+        return Response(serializer.data,status=status.HTTP_200_OK)
     def post(self, request):
         serializer = CourseSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         return Response(serializer.error,status=status.HTTP_400_BAD_REQUEST)
+
+class CourseAPIView(APIView):
+    def get(self,request,pk):
+        course=get_object_or_404(Course,course_id=pk)
+        serializer = CourseSerializer(course)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+        
+# class CodeAPIView(APIView):
+#     def get(self, request):
+#         code = Code
+
+class ProblemsAPIView(APIView):
+    def get(self,request):
+        probs = Problem.objects.all()
+        serializer = ProblemSerializer(probs,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    def post (self,request):
+        serializer=ProblemSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.data,status=status.HTTP_400_BAD_REQUEST)
+
+class ProblemAPIView(APIView):
+    def get(self,request,pk):
+        prob=get_object_or_404(Problem,prob_id=pk)
+        serializer = ProblemSerializer(prob)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+class SubmissionsAPIView(APIView):
+    def get(self,request):
+        submissions = Submission.objects.all()
+        serializer = SubmissionSerializer(submissions,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    def post(self,request):
+        serializer = SubmissionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.error,status=status.HTTP_400_BAD_REQUEST)
+
+class SubmissionAPIView(APIView):
+    def get(self,request):
+        # sid=request.GET['submit_id']
+        # uid=request.GET['user_id']
+        # pid=request.GET['prob_id']
+        # print("sid: ",sid,type(sid)," uid: ",uid," pid: ",pid)
+        serializer = None
+        if sid == "":
+            submission= Submission.objects.filter(user_id=uid)
+            serializer = SubmissionSerializer(submission,many=True)
+        else:
+            submission=get_object_or_404(Submission,submit_id=sid)
+            serializer = SubmissionSerializer(submission)
+        #submission = get_object_or_404(Submission,submit_id=sid)
+        
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+        
