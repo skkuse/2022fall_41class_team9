e6f3efb0aa7431d9195763e3ea2f7c73e0dc379a
K <netisen4@gmail.com>
2022-11-22 21:55:06 +0900

    Block user input & make class

--- src/inspection/execution.py
+++ src/inspection/execution.py
@@ -1,9 +1,75 @@
 import os
 import util
+import sys
+import math
+import time
+import importlib
 from wrapper import temp_py_handler
 
-@temp_py_handler(name="temp.py")
-def executing(code, encoding="utf-8", **kwargs):
+class Runner:
+    def __init__(self, code, encoding="utf-8"):
+        self.outputs = []
+        self.code = code
+        self.encoding = encoding
+
+    @temp_py_handler(file="runner_temp_script.py")
+    def run_script(self, **kwargs):
+        fd = kwargs['fd']
+        file = kwargs['file']
+        fd.write(self.code)
+        os.fsync(fd)
+        fd.flush()
+
+        header = util.get_py_execution_header()
+        command = util.token2command(header, file)
+
+        start = time.time()
+        out = util.execute_shell_command(command, encoding=self.encoding)
+        out = out.strip('\n')
+        end = time.time()
+
+        run_time = math.floor((end-start)*1000.*1000.)
+        self.outputs.append({"runtime":run_time,"output":out})
+
+        return out        
+
+    @temp_py_handler(file="runner_temp_soln.py")
+    def run_soln(self, *params, **kwargs):
+        fd = kwargs["fd"]
+        file = kwargs["file"]
+        name = file[:file.index('.')]
+        fd.write(self.code)
+        os.fsync(fd)
+        fd.flush()
+
+        fd2 = open("runner_temp_soln.py","r")
+        print(fd2.read())
+        fd2.close()
+
+        start = time.time()
+        answer = importlib.import_module(name)
+        out = answer.solution(*params)
+        end = time.time()
+
+        run_time = math.floor((end-start)*1000.*1000.)
+        self.outputs.append({"runtime":run_time,"output":out})
+
+        return out
+
+if __name__ == "__main__":
+    fd = open("code/code_no_error.py", "r")
+    code = fd.read()
+    fd.close()
+    runner = Runner(code, "cp949")
+    out = runner.run_soln()
+    print("================================")
+    print(out)
+    print("================================")
+
+
+'''
+@temp_py_handler(file="temp_terminal.py")
+def executing_terminal(code, encoding="utf-8", **kwargs):
     """
     execute code
     ----------------------------------------------------
@@ -17,25 +83,30 @@ def executing(code, encoding="utf-8", **kwargs):
         - @temp_py_handler는 함수 실행 전에 코드를 저장할 임시 파일을 만듭니다. 그리고 함수 실행 후 임시 파일을 지웁니다.
         - 코드에 fd, name을 kwargs로부터 가져오는 부분이 있는데 fd, name은 데코레이터로부터 자동으로 만들어집니다
     """
+
     fd = kwargs['fd']
-    name = kwargs['name']
+    file = kwargs['file']
     fd.write(code)
     os.fsync(fd)
     fd.flush()
 
     header = util.get_py_execution_header()
-    command = util.token2command(header, name)
+    command = util.token2command(header, file)
 
     out = util.execute_shell_command(command, encoding=encoding)
     out = out.strip('\n')
 
     return out
 
-if __name__ == "__main__":
-    fd = open("code_error.py", "r")
-    code = fd.read()
-    fd.close()
-    out = executing(code, encoding='cp949')
-    print("================================")
-    print(out)
-    print("================================")
+@temp_py_handler(file="temp_func.py")
+def executing_function(code, encoding='utf-8', **kwargs):
+    fd = kwargs['fd']
+    fname = kwargs['file'][:-3]
+    fd.write(code)
+    os.fsync(fd)
+    fd.flush()
+
+    exec(f"from {fname} import *")
+
+    return temp_func.solution()
+'''
--- src/inspection/wrapper.py
+++ src/inspection/wrapper.py
@@ -1,16 +1,23 @@
 import os
+import sys
+from auditor import AuditCode
 
-def temp_py_handler(name):
-    fd = open(name, "w")
+def temp_py_handler(file):
     def decorator(func):
         #print("create file")
         def wrapper(*args, **kwargs):
+            fd = open(file, "w")
+            fd.write(AuditCode.input_audit())
             kwargs['fd'] = fd
-            kwargs['name'] = name
-            out = func(*args, **kwargs)
+            kwargs['file'] = file
+            try:
+                out = func(*args, **kwargs)
+            except Exception as e:
+                out = str(type(e)) + "\n"
+                out += str(e)
             #print("delete file")
             fd.close()
-            os.remove(name)
+            os.remove(file)
             return out
         return wrapper
     return decorator
