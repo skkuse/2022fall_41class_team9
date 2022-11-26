ee3b3cfbd5b76f5231fe245c774b11b4254634ac
K <netisen4@gmail.com>
2022-11-26 16:58:04 +0900

    change return return type pandas dataframe -> dictionary

--- src/analysis/readability.py
+++ src/analysis/readability.py
@@ -1,52 +1,42 @@
-import util
+from .file import TestFileManager
+from .util import build_command, execute_shell_command
 import re
-import pandas as pd
-from file import TestFileManager
 
 BASECOMMAND = "pylama"
 
 class Auditor(TestFileManager):
-    def __init__(self, files, linters=['eradicate','mccabe','mypy','pycodestyle','pydocstyle','pyflakes','pylint','isort']):
+    def __init__(self, files, encoding='utf-8', linters=['eradicate','mccabe','mypy','pycodestyle','pydocstyle','pyflakes','pylint','isort']):
         super().__init__(files)
         self.linters = linters
+        self.encoding = encoding
         self.linter_bundle = '"' + ','.join(self.linters) + '"'
-        self.report_cols = ["file","line","col","type","content","linter"]
-        self.audit_report = pd.DataFrame(columns=self.report_cols)
+        self.empty_linter_dict = {linter:[] for linter in self.linters}
+        self.audit_report = {}
         self.analyze()
 
     def analyze(self):
         if len(self.test_dirs)+len(self.test_files) > 0:
-            command = util.token2command(BASECOMMAND, *self.test_dirs, *self.test_files, "-l", self.linter_bundle)
-
-            audit_result = util.execute_shell_command(command)
+            command = build_command(BASECOMMAND, *self.test_dirs, *self.test_files, "-l", self.linter_bundle)
+            audit_result = execute_shell_command(command, encoding=self.encoding)
             #parse result by regex & append to audit report
-            expression = r"(?P<file>.+):(?P<line>\d+):(?P<col>\d+)\s+(?P<type>[\w]+)\s(?P<content>.+)\s+\[(?P<linter>\w+)\]\n?"
+            expression = r"(?P<file>.+):(?P<line>\d+):(?P<col>\d+) (?P<type>\w*) (?P<content>.+) \[(?P<linter>\w+)\]\n?"
             regex = re.compile(expression)
             parsed_result = regex.findall(audit_result)
             #update
-            self.audit_report = pd.concat([self.audit_report, pd.DataFrame(parsed_result, columns=self.report_cols)], axis=0)
-            self.audit_report = self.audit_report.drop_duplicates()
+            for target_file, _, _, _, content, linter in parsed_result:
+                if target_file not in self.audit_report.keys():
+                    self.audit_report[target_file] = self.empty_linter_dict
+                elif content not in self.audit_report[target_file][linter]:
+                    self.audit_report[target_file][linter].append(content)
 
-    def get_report_by_file(self, file):
-        return self.audit_report[self.audit_report['file']==file].values
-
-    def get_report_by_linter(self, linter):
-        return self.audit_report[self.audit_report['linter']==linter].values
-
-    def add_file(self, file):
-        super().add_file(file)
+    def add_file(self, target_file):
+        super().add_file(target_file)
         self.analyze()
 
-    def del_file(self, file):
-        super().del_file(file)
+    def del_file(self, target_file):
+        super().del_file(target_file)
         self.analyze()
 
     def change_files(self, files):
         super().change_files(files)
         self.analyze()
-
-if __name__=="__main__":
-    auditor = Auditor(['tests','refers'])
-    print(auditor.test_dirs)
-    print(auditor.linter_bundle)
-    print(auditor.audit_report)
