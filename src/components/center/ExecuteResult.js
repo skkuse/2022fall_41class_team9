57a64758ca0837f52dc5029a973a33503e9a4f19
richcire <eric9907@naver.com>
2022-12-12 18:12:06 +0900

    show number line in terminal

--- src/components/center/ExecuteResult.js
+++ src/components/center/ExecuteResult.js
@@ -1,3 +1,4 @@
+import { style } from "@mui/system";
 import { useRecoilValue } from "recoil";
 import styled from "styled-components";
 import { actionState, executeResultState } from "../../atoms";
@@ -14,64 +15,90 @@ const ExecuteNavbar = styled.div`
   color: ${({ theme }) => theme.color};
 `;
 
-const ExecuteText = styled.div`
+const TerminalWrapper = styled.div`
   width: 100%;
   color: white;
   white-space: pre-line;
   margin: 8px;
 `;
 
+const ResultWrapper = styled.div`
+  margin-top: 20px;
+  font-size: 20px;
+`;
+const CodeBefore = styled.div`
+  background-color: ${(props) => (props.error ? "#eb4034" : "black")};
+  white-space: pre-wrap;
+`;
+const CodeMessage = styled.div`
+  background-color: #49eb71;
+`;
+const CodeAfter = styled.div`
+  white-space: pre-wrap;
+  background-color: black;
+`;
+
+const LineNumberViewer = styled.div`
+  width: 50px;
+  display: inline-block;
+`;
+const LineNumberDivider = styled.div`
+  display: inline-block;
+  margin-right: 10px;
+`;
+
+function ExecuteSuccess({ executeResult }) {
+  return <ResultWrapper>{executeResult.result}</ResultWrapper>;
+}
+
+function ExecuteFail({ executeResult }) {
+  if (!executeResult) return;
+  const codeLst = executeResult.code.split("\n");
+  console.log(codeLst);
+  const errorLine = executeResult.linePos;
+  const errorMessage = executeResult.result;
+  const errorBefore = codeLst.slice(0, errorLine);
+  const errorAfter = codeLst.slice(errorLine);
+
+  return (
+    <ResultWrapper>
+      {errorBefore.map((element, index) => (
+        <>
+          <CodeBefore error={index + 1 === Number(errorLine)} key={index}>
+            <LineNumberViewer>{`${index}`}</LineNumberViewer>
+            <LineNumberDivider>|</LineNumberDivider>
+            {element === "" ? "\n" : element}
+          </CodeBefore>
+        </>
+      ))}
+      <CodeMessage>{errorMessage}</CodeMessage>
+      {errorAfter.map((element, index) => (
+        <CodeAfter key={index}>
+          <LineNumberViewer>{`${errorLine + index}`}</LineNumberViewer>
+          <LineNumberDivider>|</LineNumberDivider>
+          {element === "" ? "\n" : element}
+        </CodeAfter>
+      ))}
+    </ResultWrapper>
+  );
+}
+
 function ExecuteResult() {
   const action = useRecoilValue(actionState);
   const executeResult = useRecoilValue(executeResultState);
 
-  const showExecuteSuccess = () => {
-    if (executeResult) {
-      return <div>{executeResult.result}</div>;
-    }
-  };
-  const showExecuteFail = () => {
-    if (executeResult) {
-      const userCode = executeResult.code;
-      const codeLst = userCode.split("\n");
-      const errorLine = executeResult.linePos;
-      const errorMessage = executeResult.result;
-      const errorBefore = codeLst.slice(0, errorLine);
-      const errorAfter = codeLst.slice(errorLine);
-      return (
-        <>
-          {errorBefore.map((element, index) => (
-            <div
-              key={index}
-              style={{
-                backgroundColor:
-                  index + 1 === Number(errorLine) ? "#E67BA4" : "black",
-                whiteSpace: "pre-wrap",
-              }}
-            >
-              {element}
-            </div>
-          ))}
-          <div style={{ backgroundColor: "#72CC82" }}>{errorMessage}</div>
-          {errorAfter.map((element, index) => (
-            <div style={{ whiteSpace: "pre-wrap" }} key={index}>
-              {element}
-            </div>
-          ))}
-        </>
-      );
-    }
-  };
   return (
     <ExecuteResultContainer action={action}>
       <ExecuteNavbar>실행결과</ExecuteNavbar>
-      <ExecuteText>
+      <TerminalWrapper>
         Jser@Terminal ~ %
         <br />
-        {executeResult && executeResult.status === "success"
-          ? showExecuteSuccess()
-          : showExecuteFail()}
-      </ExecuteText>
+        {executeResult && executeResult.status === "success" ? (
+          <ExecuteSuccess executeResult={executeResult} />
+        ) : (
+          <ExecuteFail executeResult={executeResult} />
+        )}
+      </TerminalWrapper>
     </ExecuteResultContainer>
   );
 }
