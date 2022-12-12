1b9be4d89fbd04717047593cdbaab7ee5e3f42c1
sachoi1201 <61404276+sachoi1201@users.noreply.github.com>
2022-12-12 13:21:38 +0900

    centerfooter 주석

--- src/components/center/centerFooter/MainButtons.js
+++ src/components/center/centerFooter/MainButtons.js
@@ -28,16 +28,26 @@ function MainButtons({
   setResetDialogOpen,
   setIsDataLoading,
 }) {
+  // 현재 작업 중인 editor에 저장된 code에 관한 state
   const userCode = useRecoilValue(testState);
+  // 사용자에 관한 state
   const userInfo = useRecoilValue(userState);
+  // 현재 작업중인 editor에 관한 state
   const savePart = useRecoilValue(savePartState);
+  // 문제 정보에 관한 state
   const currentProblemInfo = useRecoilValue(currentProblemInfoState);
+  // 코드 불러오기, 초기화, 복사, 다운로드에 관한 state
   const setAction = useSetRecoilState(actionState);
+  // 실행 결과에 관한 state
   const setExecuteResult = useSetRecoilState(executeResultState);
+  // 채점 결과에 관한 state
   const setGradingResult = useSetRecoilState(gradingResultState);
+  // 제출 결과에 관한 state
   const setSubmitResult = useSetRecoilState(submitResultState);
+  // 제출 실행 결과 유무에 관한 state
   const [doneSubmit, setDoneSubmit] = useRecoilState(doneSubmitState);
 
+  // 코드 실행
   const { mutate: executeMutate } = useMutation(
     () => executeCode({ user_code: userCode }),
     {
@@ -48,6 +58,7 @@ function MainButtons({
       onError: (error) => console.log(error),
     }
   );
+  // 코드 채점
   const { mutate: gradingMutate } = useMutation(
     () => gradeCode(userCode, currentProblemInfo.prob_id),
     {
@@ -58,7 +69,7 @@ function MainButtons({
       onError: (error) => console.log(error),
     }
   );
-
+  // 코드 제출
   const { mutate: submitMutate } = useMutation(
     (_) =>
       submitCode({
@@ -70,37 +81,37 @@ function MainButtons({
       onError: (error) => console.log(error),
     }
   );
-
+  // 제출 결과 받기
   const getSubmissionResult = async (submitId) => {
     try {
       const response = await axios.get(`/onlinejudge/analysis2/${submitId}`);
-      console.log(response.data);
+      // console.log(response.data);
       setIsDataLoading(false);
       setSubmitResult(response.data);
       setDoneSubmit(true);
       setAction("submit");
     } catch (error) {
-      console.log(error);
+      // console.log(error);
       setIsDataLoading(false);
       setSubmitResult(ERROR_CODE_RESULT);
       setDoneSubmit(true);
       setAction("submit");
     }
   };
-
+  // 실행 버튼 클릭
   const handleExecuteBtnClick = () => {
     localStorage.setItem(savePart, userCode);
     setAction("execute");
     executeMutate();
   };
-
+  // 채점 버튼 클릭
   const handleGradingClick = () => {
     localStorage.setItem(savePart, userCode);
     setAction("grading");
-
     gradingMutate();
   };
 
+  // 제출 버튼 클릭
   const handleSubmitBtnClick = async () => {
     setSubmitDialogOpen(true);
     setIsDataLoading(true);
@@ -111,13 +122,14 @@ function MainButtons({
       },
     });
   };
-
+  // 다시 풀어보기 버튼 클릭
   const handleRestartBtnClick = () => {
     setResetDialogOpen(true);
   };
 
   return (
     <FooterBtns>
+      {/* 제출 버튼을 클릭했다면 doneSubmit, 아니라면 실행, 채점, 제출 버튼을 보여준다/ */}
       {doneSubmit ? (
         <Button
           color="primary"
